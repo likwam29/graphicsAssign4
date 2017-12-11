d9b48438ebde63549adeb688b3d34ed97438c7cd
likwam29 <deftblow34@gmail.com>
2017-12-10 18:38:07 -0600

    wrote instructions

--- game/the-game.js
+++ game/the-game.js
@@ -1,44 +1,47 @@
 // the-game.js
+// Matthew likwarz and Sean Mitchell
+// GGW: Procedural texture on villain. Special item generation at specific point in game. Multi-key movement for a better gaming experience,
+// sound effects during hero villain collision and item collection so the user knows when contact has been made.
 var gl;
-var canvas; 
+var canvas;
 const WALLHEIGHT     = 70.0; // Some playing field parameters
 const ARENASIZE      = 1000.0;
 const EYEHEIGHT      = 15.0;
 const HERO_VP        = 0.625;
 
-const  upx=0.0, upy=1.0, upz=0.0;    // Some LookAt params 
+const  upx=0.0, upy=1.0, upz=0.0;    // Some LookAt params
 
-const fov = 60.0;     // Perspective view params 
+const fov = 60.0;     // Perspective view params
 const near = 1.0;
 const far = 10000.0;
 var aspect, eyex, eyez;
 
-const width = 1000;       // canvas size 
+const width = 1000;       // canvas size
 const height = 625;
-const vp1_left = 0;      // Left viewport -- the hero's view 
+const vp1_left = 0;      // Left viewport -- the hero's view
 const vp1_bottom = 0;
 
 // Lighting stuff
-var la0  = [ 0.2,0.2,0.2, 1.0 ]; // light 0 ambient intensity 
-var ld0  = [ 1.0,1.0,1.0, 1.0 ]; // light 0 diffuse intensity 
-var ls0  = [ 1.0,1.0,1.0, 1.0 ]; // light 0 specular 
-var lp0  = [ 0.0,1.0,1.0, 1.0 ]; // light 0 position -- will adjust to hero's viewpoint 
-var ma   = [ 0.02 , 0.2  , 0.02 , 1.0 ]; // material ambient 
-var md   = [ 0.08, 0.6 , 0.08, 1.0 ]; // material diffuse 
-var ms   = [ 0.06  , 0.7, 0.06  , 1.0 ]; // material specular 
-var me      =100;             // shininess exponent 
+var la0  = [ 0.2,0.2,0.2, 1.0 ]; // light 0 ambient intensity
+var ld0  = [ 1.0,1.0,1.0, 1.0 ]; // light 0 diffuse intensity
+var ls0  = [ 1.0,1.0,1.0, 1.0 ]; // light 0 specular
+var lp0  = [ 0.0,1.0,1.0, 1.0 ]; // light 0 position -- will adjust to hero's viewpoint
+var ma   = [ 0.02 , 0.2  , 0.02 , 1.0 ]; // material ambient
+var md   = [ 0.08, 0.6 , 0.08, 1.0 ]; // material diffuse
+var ms   = [ 0.06  , 0.7, 0.06  , 1.0 ]; // material specular
+var me      =100;             // shininess exponent
 
 //Lighting stuff for the second light
 var la02  = [ 0.5,0.1,0.2, 1.0 ]; // light 0 ambient intensity
-var ld02  = [ 1.0,1.0,1.0, 1.0 ]; // light 0 diffuse intensity 
-var ls02  = [ 0.0,0.20,0.0, 1.0 ]; // light 0 specular 
-var lp02  = [ 0.0,1.0,1.0, 1.0 ]; // light 0 position -- will adjust to hero's viewpoint 
-var me2      = 1;             // shininess exponent 
+var ld02  = [ 1.0,1.0,1.0, 1.0 ]; // light 0 diffuse intensity
+var ls02  = [ 0.0,0.20,0.0, 1.0 ]; // light 0 specular
+var lp02  = [ 0.0,1.0,1.0, 1.0 ]; // light 0 position -- will adjust to hero's viewpoint
+var me2      = 1;             // shininess exponent
 
 
-const red  = [ 1.0,0.0,0.0, 1.0 ]; // pure red 
-const blue = [ 0.0,0.0,1.0, 1.0 ]; // pure blue 
-const green = [ 0.0,1.0,0.0, 1.0 ]; // pure green 
+const red  = [ 1.0,0.0,0.0, 1.0 ]; // pure red
+const blue = [ 0.0,0.0,1.0, 1.0 ]; // pure blue
+const green = [ 0.0,1.0,0.0, 1.0 ]; // pure green
 const yellow = [ 1.0,1.0,0.0, 1.0 ]; // pure yellow
 
 
@@ -68,13 +71,13 @@ var timerVal = 60;
 
 window.onload = function init(){
     canvas = document.getElementById( "gl-canvas" );
-    
+
     //    gl = WebGLUtils.setupWebGL( canvas );
     gl = WebGLDebugUtils.makeDebugContext( canvas.getContext("webgl") ); // For debugging
     if ( !gl ) { alert( "WebGL isn't available" ); }
-    
+
     //  Configure WebGL
-    
+
 	// This will dictate the sky color
     gl.clearColor( 0.0, 0.75, 1.0, 1.0 );
 	gl.enable(gl.DEPTH_TEST);
@@ -96,15 +99,15 @@ window.onload = function init(){
                      // shader.  If your game object uses it, be sure
                      // to switch it back to 0 for consistency with
                      // those objects that use the defalt.
-    
-    
+
+
     arena = new Arena(program);
     arena.init();
 
     hero = new Hero(program, eyex, 0.0, eyez, 45, 10.0);
     hero.init();
-		
-	var arenaX = ratioCreator();	
+
+	var arenaX = ratioCreator();
 	var arenaZ = ratioCreator() * -1;
     thingSeeking2 = new ThingSeeking2(program, arenaX, 0.0, arenaZ, 0, 10.0);
     thingSeeking2.init();
@@ -114,7 +117,7 @@ window.onload = function init(){
 
     villain = new Villain(program, (3*ARENASIZE)/4.0, 0.0, -ARENASIZE/4.0, 0, 10.0);
     villain.init();
-    
+
     render();
 };
 
@@ -131,18 +134,18 @@ function ratioCreator(){
 	if(first > 1 && first == second){
 		first--;
 	}
-	
+
 	return ARENASIZE*first/second;
 }
 
 function createItems(numItems){
 	var retArray = [];
 	for(var i=0; i<numItems; i++){
-		
+
 		var arenaX = ratioCreator();
-		
+
 		var arenaZ = ratioCreator() * -1;
-		
+
 		var temp = new ThingSeeking(program, arenaX, 0.0, arenaZ, 0, 10.0);
 		temp.init();
 		retArray.push(temp);
@@ -154,16 +157,16 @@ function itemCollision(){
 	// if hero.x is within thingSeeking.x +/- thingSeeking.bounding
 	// And if hero.z is within thingSeeking.z +/- thingSeeking.bounding
 	// then collision has happend
-	
+
 	for(var i=0; i<totalItems; i++){
 		var thingX = thingSeeking[i].x;
 		var thingZ = thingSeeking[i].z;
 		var bound = thingSeeking[i].bounding_cir_rad;
 		if(hero.x >= (thingX - bound) && hero.x <= (thingX + bound) && hero.z <= (thingZ + bound) && hero.z >= (thingZ - bound)){
-			
+
 			heroCounter++;
 			//thingSeeking[i].isHit = true;
-			/* 
+			/*
 			 we have to change the x and z of the object just hit because it still
 			 exists on the page and will continue to hit it if we sit in the spot where it was
 			 causing the counter to keep running
@@ -171,7 +174,7 @@ function itemCollision(){
 			var arenaX = ratioCreator();
 
 			var arenaZ = ratioCreator() * -1;
-			
+
 			if ((heroCounter) % 5 == 0){
 			//If the object is a special object then add 5 to the total
 				if(thingSeeking[i].hasTexture)
@@ -195,7 +198,7 @@ function itemCollision(){
 			audio.play();
 			break;
 		}
-		
+
 		if(villain.x >= (thingX - bound) && villain.x <= (thingX + bound) && villain.z <= (thingZ + bound) && villain.z >= (thingZ - bound)){
 			villainCounter++;
 			//If the object is a special object then add 5 to the total
@@ -207,10 +210,10 @@ function itemCollision(){
 			thingSeeking[i].x = ratioCreator();
 			thingSeeking[i].z = ratioCreator() * -1;
 			document.getElementById("villainScore").innerHTML = villainCounter;
-			
+
 		}
 	}
-	
+
 }
 
 function heroWallCollision(){
@@ -224,19 +227,19 @@ function heroWallCollision(){
 		hero.xdir = (hero.xdir * -1);
 		hero.x = 10;
 	}
-	
+
 	// botton wall collision
 	if(hero.z > -3){
 		hero.zdir = (hero.zdir * -1);
 		hero.z = -10;
 	}
-	
+
 	// top wall collision
 	if(hero.z < -998){
 		hero.zdir = (hero.zdir * -1);
 		hero.z = -988;
 	}
-	
+
 	// right wall collision
 	if(villain.x > 997){
 		villain.xdir = (villain.xdir * -1);
@@ -247,13 +250,13 @@ function heroWallCollision(){
 		villain.xdir = (villain.xdir * -1);
 		villain.x = 10;
 	}
-	
+
 	// botton wall collision
 	if(villain.z > -3){
 		villain.zdir = (villain.zdir * -1);
 		villain.z = -10;
 	}
-	
+
 	// top wall collision
 	if(villain.z < -998){
 		villain.zdir = (villain.zdir * -1);
@@ -285,31 +288,31 @@ function villainMove(){
 	var moveRate = 3.8;
 	var negX = true;
 	var negZ = true;
-	
+
 	var currX = villain.x;
 	var currZ = villain.z;
-	
+
 	// this will make it so the villain grabs all the items in order.
 	var nextVictim = villainCounter % totalItems;
-	
+
 	var closestX = thingSeeking[nextVictim].x;
 	var closestZ = thingSeeking[nextVictim].z;
 	// move towards closest x value, either positive or negative
-	
+
 	// move x and z closer to that item.
-	
+
 	if(currX > closestX){
 		negX = true;
 	}else{
 		negX = false;
 	}
-	
+
 	if(currZ > closestZ){
 		negZ = true;
 	}else{
 		negZ = false;
 	}
-	
+
 	if(negX){
 		// move left
 		villain.x += (moveRate) * neg;
@@ -317,9 +320,9 @@ function villainMove(){
 		// move right
 		villain.x += moveRate;
 	}
-	
+
 	// move toward closest z value, either positive or negative
-	
+
 	if(negZ){
 		// move up
 		villain.z += (moveRate) * neg;
@@ -331,21 +334,21 @@ function villainMove(){
 
 function render()
 {
-	
+
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	
+
 	// this needs to be here so while holding movement key it still moves.
 	executeMovement();
-	
+
 	itemCollision();
 	heroVillianCollision()
 	heroWallCollision();
-	
+
 	villainMove();
-    
-    // Hero's eye viewport 
+
+    // Hero's eye viewport
     gl.viewport( vp1_left, vp1_bottom, (HERO_VP * width), height );
-    
+
     lp0[0] = hero.x + hero.xdir; // Light in front of hero, in line with hero's direction
     lp0[1] = EYEHEIGHT;
     lp0[2] = hero.z + hero.zdir;
@@ -356,17 +359,17 @@ function render()
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
     arena.show();
-	
+
 	hero.show();
-	
+
     for(var i=0; i<totalItems; i++){
 		thingSeeking[i].show();
 	}
-	
-  
+
+
 	villain.show();
-	
-    // Overhead viewport 
+
+    // Overhead viewport
     var horiz_offset = (width * (1.0 - HERO_VP) / 20.0);
     gl.viewport( vp1_left + (HERO_VP * width) + horiz_offset ,
 		 vp1_bottom, 18 * horiz_offset, height );
@@ -378,24 +381,24 @@ function render()
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
     arena.show();
     hero.show();
-	
+
     for(var i=0; i<totalItems; i++){
 		thingSeeking[i].show();
 	}
-	
+
 	villain.show();
-	
-	
+
+
 	if(runGame){
 		requestAnimFrame( render );
 	}
-    
+
 };
 
 // Key listener
 
 var keyMap = [];
-document.addEventListener("keydown", onDocumentKeyDown, true); 
+document.addEventListener("keydown", onDocumentKeyDown, true);
 document.addEventListener("keyup", onDocumentKeyUp, true);
 
 // the start button click listener
@@ -403,19 +406,19 @@ document.getElementById("startGameButton").addEventListener("click", gameStart);
 
 // This will start/stop the game
 function gameStart(){
-	
+
 	// if the game has finished change the click functionality
 	if (timerVal < 0){
 		location.reload();
 	}
-	
+
 	// if the game is not in progress then start it
 	if(!runGame){
 		runGame = !runGame;
 		render();
 		onTimer();
 	}
-	
+
 }
 
 
@@ -465,4 +468,3 @@ function executeMovement(){
 		hero.move(-2.0);
 	}
 }
-
